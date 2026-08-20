317e58802edf118e73051524aeb48f8b6acf8b63
Lovanjatovo <hei.lovanjatovo@gmail.com>
2026-08-20 10:11:08 +0300

    first commit

---
+++ LovaN2Allan/src/main/java/com/example/LovaN2Allan/LovaN2AllanApplication.java
@@ -0,0 +1,13 @@
+package com.example.LovaN2Allan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovaN2AllanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovaN2AllanApplication.class, args);
+	}
+
+}
